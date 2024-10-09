109ce4a37c4f530e56aa8069f8f089c6792c3b06
Baman Irani <amany29074@gmail.com>
2024-10-09 19:57:49 +0530

    commit test6

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-export  const Test = () => {
-  return (
-    <div  className='text-red-600 text-5xl'>Test
-    
-    
+export const Test = () => {
+    return (
+        <div className='text-red-600 text-5xl'>Test
 
-        <h1>Added h1 tag for test 2</h1>
-        <h1>Added h1 tag for test 3</h1>
-        <h1>Added h1 tag for test 4</h1>
-        <h1>Added h1 tag for test 5
+
+
+            <h1>Added h1 tag for test 2</h1>
+            <h1>Added h1 tag for test 3</h1>
+            <h1>Added h1 tag for test 4</h1>
+            <h1>Added h1 tag for test 5</h1>
+            <h1>Added h1 tag for test 6</h1>
+
             
-        </h1>
-    </div>
-  )
+        </div>
+    )
 }
- 
