56582af77c91d3d08baef4972c14dfe42625116b
Baman Irani <amany29074@gmail.com>
2024-10-09 19:14:58 +0530

    added new line in test

--- src/Test.jsx
+++ src/Test.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 export  const Test = () => {
   return (
-    <div  className='text-red-600 text-5xl'>Test</div>
+    <div  className='text-red-600 text-5xl'>Test
+    
+    
+
+        <h1>Added h1 tag for test 2</h1>
+    </div>
   )
 }
  
