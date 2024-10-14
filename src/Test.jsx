f8ada66e875aa9a9af0c988bb10e36fd6f22c5bc
Baman Irani <amany29074@gmail.com>
2024-10-14 21:35:36 +0530

    new 2 features

--- src/Test.jsx
+++ src/Test.jsx
@@ -7,7 +7,6 @@ export const Test = () => {
 
 
             <h1>Added h1 tag for test 2</h1>
-            <h1>its  Github pr review test</h1>
             <h1>its a github ai pr review test</h1>
             
            
